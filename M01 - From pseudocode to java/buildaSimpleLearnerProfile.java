a8f1e968b11abceb87d3291bb08a4d2784c55541
VARUN S M <varunsm15@gmail.com>
2026-08-12 15:30:27 +0530

    Build a Simple Learner Profile

---
+++ M01 - From pseudocode to java/buildaSimpleLearnerProfile.java
@@ -0,0 +1,17 @@
+
+public class buildaSimpleLearnerProfile {
+
+    public static void main(String[] args) {
+// Write your code here.
+        String learnerName = "Ananya";
+        int completedProblems = 25;
+        double practiceHours = 2.5;
+        boolean projectCompleted = true;
+
+        System.out.println("Learner: " + learnerName);
+        System.out.println("Problems Completed: " + completedProblems);
+        System.out.println("Practice Hours: " + practiceHours);
+        System.out.println("Project Completed: " + projectCompleted);
+
+    }
+}
