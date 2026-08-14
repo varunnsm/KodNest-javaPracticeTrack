fc7438b81b1524efab2ffb9193081bfc1d4923bd
VARUN S M <varunsm15@gmail.com>
2026-08-14 15:25:30 +0530

    Cascading Shift With Mixed Input

---
+++ M01 - From pseudocode to java/T03 - Console input with scanner/1_cascadingShiftWithMixedInput.java
@@ -0,0 +1,23 @@
+// Cascading shift with mixed input
+
+import java.util.Scanner;
+
+public class cascadingShiftWithMixedInput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        String fullName = scanner.nextLine();
+        String city = scanner.nextLine();
+
+        System.out.println("Name: " + fullName);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+
+        scanner.close();
+
+    }
+
+}
