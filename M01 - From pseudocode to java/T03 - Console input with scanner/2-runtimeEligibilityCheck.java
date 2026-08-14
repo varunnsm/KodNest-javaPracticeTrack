01768722b9e60cca60761337aab813c79d387b66
VARUN S M <varunsm15@gmail.com>
2026-08-14 15:55:34 +0530

    Runtime Eligibility Check

---
+++ M01 - From pseudocode to java/T03 - Console input with scanner/2-runtimeEligibilityCheck.java
@@ -0,0 +1,24 @@
+// Runtime Eligibility Check
+
+import java.util.Scanner;
+
+public class runtimeEligibilityCheck {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int codingScore = scanner.nextInt();
+        int attendance = scanner.nextInt();
+        int projects = scanner.nextInt();
+
+        // Build the correctly grouped eligibility condition.
+        // Print Eligible or Not Eligible.
+        if (codingScore >= 60 && attendance >= 75 || projects >= 2) {
+            System.out.println("Eligible");
+        } else {
+            System.out.println("Not Eligible");
+        }
+        scanner.close();
+    }
+
+}
