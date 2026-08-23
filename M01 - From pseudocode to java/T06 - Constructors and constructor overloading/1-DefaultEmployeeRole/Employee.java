075002554bf808c1ea8e37084c5e8404f72ad44e
VARUN S M <varunsm15@gmail.com>
2026-08-23 21:43:32 +0530

    Default Employee Role

---
+++ M01 - From pseudocode to java/T06 - Constructors and constructor overloading/1-DefaultEmployeeRole/Employee.java
@@ -0,0 +1,16 @@
+
+class Employee {
+
+    String name;
+    String role;
+
+    Employee() {
+        this.name = name;
+        this.role = "Trainee";
+    }
+
+    void display() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee Role: " + role);
+    }
+}
---
+++ M01 - From pseudocode to java/T06 - Constructors and constructor overloading/1-DefaultEmployeeRole/Main.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Employee obj = new Employee();
+
+        obj.name = sc.nextLine();
+
+        obj.display();
+
+    }
+}
