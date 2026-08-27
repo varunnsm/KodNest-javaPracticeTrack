3bc6e2ccd21f1c1379b7497c24cf2eefb2558ccd
VARUN S M <varunsm15@gmail.com>
2026-08-27 16:49:39 +0530

    Replace Matching Values

--- M01 - From pseudocode to java/T08 - Array and Matrix/2-ReplaceMatchingValues.java
+++ M01 - From pseudocode to java/T08 - Array and Matrix/2-ReplaceMatchingValues.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Main {
+public class ReplaceMatchingValues {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
